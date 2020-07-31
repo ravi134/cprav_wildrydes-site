df4247ba670e3f2701c06501b83da5694fbc38ab
Ravi VM ubuntu1804NEW <ravi@anumula.net>
2020-07-31 00:33:20 -0700

    Copied wildrydes-site

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_n485j70oa', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '45chghvt5f7mp5f6qccokotp3r', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://bg16igd5sd.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
